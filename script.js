25e190de81284f0346a7d06a30daad911edf51d3
eletrixgg <diyonthomas00@gmail.com>
2024-03-29 12:45:23 +1100

    bigdowngrade[probanyk

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAG-N5Vn9nw4MRfGg13gSOlTLAkjmpJU1I",
-  authDomain: "randomchat1200.firebaseapp.com",
-  databaseURL: "https://randomchat1200-default-rtdb.firebaseio.com",
-  projectId: "randomchat1200",
-  storageBucket: "randomchat1200.appspot.com",
-  messagingSenderId: "388785710092",
-  appId: "1:388785710092:web:037248699fd99f9b2ffa29"
-};
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
 let username = null;
 
 function setUsername() {
@@ -47,15 +33,6 @@ function displayMessage(chatId, message) {
   }
 }
 
-function displayPrivateMessage(message) {
-  const privateChatMessages = document.getElementById('privateChatMessages');
-  if (privateChatMessages) {
-    const messageElement = document.createElement('li');
-    messageElement.textContent = `${message.sender} (to ${message.recipient}): ${message.message}`;
-    privateChatMessages.appendChild(messageElement);
-  }
-}
-
 function setupChatListener(chatId) {
   const chatRef = database.ref(`chats/${chatId}`);
   chatRef.on('child_added', (snapshot) => {
@@ -64,17 +41,5 @@ function setupChatListener(chatId) {
   });
 }
 
-function setupPrivateChatListener() {
-  const privateChatRef = database.ref('chats/private');
-  privateChatRef.on('child_added', (snapshot) => {
-    const message = snapshot.val();
-    if ((message.recipient === username && message.sender === recipientUsername) || 
-        (message.sender === username && message.recipient === recipientUsername)) {
-      displayPrivateMessage(message);
-    }
-  });
-}
-
 setupChatListener('globalChat1');
 setupChatListener('globalChat2');
-setupPrivateChatListener();
